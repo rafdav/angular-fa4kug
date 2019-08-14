6e534f11bd2282476cb086487dc7f6a280905d4b
rafdav <rafdavmar@gmail.com>
2019-08-14 16:32:52 -0500

    2 files modified, 2 added, 1 deleted

--- src/app/consulta.component.ts
+++ src/app/consulta.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  selector: 'consulta',
+  templateUrl: 'consulta.component.html',
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class ConsultaComponent  {
   @Input() name: string;
 }
