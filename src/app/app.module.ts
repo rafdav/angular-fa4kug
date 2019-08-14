e339bcea2cdbc5c5d61885f7ed64b7e27d23f0ca
rafdav <rafdavmar@gmail.com>
2019-08-14 16:58:54 -0500

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+//import {MatTabsModule} from '@angular/material/tabs';
+//import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { ConsultaComponent } from './consulta.component';
